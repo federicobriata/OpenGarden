0f03e187ba4b71bc39debac52ef8929378a96452
Federico Briata <federico@briata.org>
2022-01-13 19:07:56 +0100

    fix mem leak, now it's fully working in off-line mode

--- examples/Full Examples/Actuators_Sensors_Indoor_Outdoor_Yun/Actuators_Sensors_Indoor_Outdoor_Yun.ino
+++ examples/Full Examples/Actuators_Sensors_Indoor_Outdoor_Yun/Actuators_Sensors_Indoor_Outdoor_Yun.ino
@@ -350,12 +350,11 @@ void loop() {
 
                 // Read incoming bytes from the server
                 int cont=0;
-                char recv[8];
+                char recv[8] = {'0', '0', '0', '0', '0', '0', '0', '\0'};
                 while (www.available()>0) {
                     recv[cont] = www.read();
                     cont++;
                 }
-                recv[cont]='\0';
                 updateURL ="";
                 www.close();
                 DEBUG_PRINTLN();
